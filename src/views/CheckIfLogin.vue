07d6929f821a8e8c4e1557023800a8eaa974eb9e
Thijmen <yeaaccount@gmail.com>
2023-02-24 12:08:51 +0100

    checkiflogin now redirects the user if logged in or not

---
+++ src/views/CheckIfLogin.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+export default {
+
+  mounted() {
+
+
+    axios.get('/checklogin')
+      .then((response) => {
+    console.log("yoo")
+
+
+
+
+        if (response.status == 200) {
+          this.$router.push('/profile/?user='+response.data)
+
+        }
+
+
+
+      })
+      .catch((error) => {
+        this.$router.push('/home')
+      });
+  }
+}
+</script>
