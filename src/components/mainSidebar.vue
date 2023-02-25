bba8185f9f506c5c58b6d72f0dfb7ac6a71786df
Thijmen <yeaaccount@gmail.com>
2023-02-25 01:01:02 +0100

    added a sidebar for the app

---
+++ src/components/mainSidebar.vue
@@ -0,0 +1,105 @@
+<template>
+  <!-- TODO: MAKE TEXT DISAPEAR WHEN the preview becomes too small -->
+  <aside id="logo-sidebar" class=" w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <a  class="flex items-center pl-2.5 mb-5">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-7" alt="= Logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Zenox</span>
+      </a>
+      <ul class="space-y-2">
+        <li>
+          <a @click="$router.push('/feed')"
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <svg aria-hidden="true"
+              class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+            </svg>
+            <span class="ml-3">Feed</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <svg aria-hidden="true"
+              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z">
+              </path>
+              <path
+                d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
+              </path>
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Messages</span>
+            <span
+              class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
+          </a>
+        </li>
+        <li>
+          <a @click=profilePage()
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <svg aria-hidden="true"
+              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd">
+              </path>
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Profile</span>
+          </a>
+        </li>
+        <li>
+          <a @click=logout()
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <svg width="18px" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true">
+              <path fill= "#636363" clip-rule="evenodd" fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM6.75 9.25a.75.75 0 000 1.5h4.59l-2.1 1.95a.75.75 0 001.02 1.1l3.5-3.25a.75.75 0 000-1.1l-3.5-3.25a.75.75 0 10-1.02 1.1l2.1 1.95H6.75z">
+              </path>
+            </svg>
+            <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
+          </a>
+        </li>
+
+
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Sidebar",
+  methods: {
+    profilePage() {
+      console.log("YOO")
+
+      axios.get('/checklogin')
+        .then((response) => {
+
+          if (response.status == 200) {
+            this.$router.push('/profile/?user=' + response.data) .then(() => {
+    console.log('Updated route', this.$route)
+    // process the updated route params
+  })
+
+
+
+          }
+        })
+    },
+    logout() {
+      axios.get('/logout').then((respose) => {
+        this.$router.push('/')
+      });
+    }
+
+  }
+};
+</script>
+
