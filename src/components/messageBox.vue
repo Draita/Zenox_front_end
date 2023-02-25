8527e94838e1609856d23526407db8a5ecaf035e
Thijmen <yeaaccount@gmail.com>
2023-02-25 01:01:33 +0100

    A box to send messages

---
+++ src/components/messageBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex justify-center items-center">
+      <div class="w-full max-w-md">
+        <form @submit.prevent="sendMessage" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="message">
+              Message
+            </label>
+            <textarea v-model="message" id="message" name="message" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="4"></textarea>
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        message: '',
+      }
+    },
+    methods: {
+      sendMessage() {
+        const data = {
+          content: this.message,
+          location: 'feed',
+        };
+        axios.post('/messages', data)
+          .then(() => {
+            console.log('🤡 Message sent!');
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        this.message = '';
+      }
+    }
+  }
+  </script>
