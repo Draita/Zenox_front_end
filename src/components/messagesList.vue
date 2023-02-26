d1dfe4b440f69c8a76f9b6034204bb8171b74811
Thijmen <yeaaccount@gmail.com>
2023-02-26 01:19:03 +0100

    added a message list componement to make a list of all the messages

---
+++ src/components/messagesList.vue
@@ -0,0 +1,47 @@
+<template>
+    <message v-for="message in messages" :message="message" :editable="editable" :key="message._id"
+        :username="this.username" :url="this.Url" />
+</template>
+
+<script>
+import config from "@/config";
+import Message from "@/components/Message.vue";
+import axios from "axios";
+
+
+export default {
+    components: {
+        Message,
+    },
+    props: {
+        messages: {
+            type: Object,
+            required: true
+        },
+
+        editable:{
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+            Url: config.apiUrl,
+            username: ""
+
+        };
+    },
+    mounted() {
+        axios
+            .get('/profile/username')
+            .then((response) => {
+                this.username = response.data;
+            })
+    },
+    methods: {
+
+
+    }
+}
+</script>
