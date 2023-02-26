e0cabbe6bbaa3a981cf6b81ff5d0791cbda489cc
Thijmen <yeaaccount@gmail.com>
2023-02-26 01:19:22 +0100

    added a discovery tab where all the messages are shown

---
+++ src/views/Discover.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container-fluid  flex  flex-row items-center">
+        <main-sidebar />
+        <div class="flex flex-col md:flex-row h-screen flex-1 overflow-scroll">
+            <div class="profile min-w-full flex flex-col items-center">
+                <!-- <message-box class="min-w-full" /> -->
+                <div class="flex flex-col flex-1 mx-4 md:mx-16">
+                    <h2 class="text-lg font-semibold mb-4">Discover posts from other users</h2>
+                    <messages-list :messages="messages">
+                    </messages-list>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import MainSidebar from "@/components/MainSidebar.vue";
+import MessageBox from "@/components/MessageBox.vue";
+import Message from "@/components/Message.vue";
+import messagesList from "@/components/messagesList.vue";
+
+import axios from "axios";
+
+
+
+
+export default {
+    components: {
+        MainSidebar,
+        MessageBox,
+        Message,
+        messagesList
+    },
+    data() {
+        return {
+            messages: [],
+
+        };
+    },
+    mounted() {
+        axios
+            .get('/messages/discover')
+            .then((response) => {
+                this.messages = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+};
+</script>
