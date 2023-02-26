b69de55216ecddf4477b6dbc51f173044f7b18d1
Thijmen <yeaaccount@gmail.com>
2023-02-26 14:39:59 +0100

    deleted routing

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,17 @@ import { createRouter, createWebHistory} from 'vue-router'
 import Home from './views/home.vue'
 import Menu from './views/menu.vue'
 import AboutMe from './views/AboutMe.vue'
-import DigitalArt from './views/DigitalArt.vue'
 import Login from './views/login.vue'
 import CheckLogin from './views/CheckIfLogin.vue'
 import Register from './views/Register.vue'
 import Profile from './views/Profile.vue'
+import Discover from './views/Discover.vue'
+import Dev from './views/Dev.vue'
 import Feed from './views/Feed.vue'
 
-
-
-
-
 import '@/axios'
+
+
 
 
 
@@ -29,15 +28,18 @@ const router = createRouter({
     history: createWebHistory(),
 
     routes: [
-        {path: '/home', name: 'home', component: Home},
+        // {path: '/home', name: 'home', component: Home},
         {path: '/login', name: 'login', component: Login},
         {path: '/', name: 'checkLogin', component: CheckLogin},
         {path: '/menu', name: 'menu', component: Menu},
         {path: '/about-me', name: 'AboutMe', component: AboutMe},
-        {path: '/digital-art', name: 'DigalArt', component: DigitalArt},
         {path: '/register', name: 'Register', component: Register},
-        {path: '/Profile', name: 'Profile', component: Profile},
         {path: '/Feed', name: 'Feed', component: Feed},
+        {path: '/Discover', name: 'Discover', component: Discover},
+        {path: '/Dev', name: 'Dev', component: Dev},
+        {path: '/Profile', name: 'Profile', component: Profile},
+
+
 
 
 
