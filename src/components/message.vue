f129eefe33290379030077f38af7466adc3ea3d3
Thijmen <yeaaccount@gmail.com>
2023-02-27 19:41:27 +0100

    added reploy button to message

--- src/components/message.vue
+++ src/components/message.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-show = "show" class="bg-white rounded-lg shadow-lg mb-4 relative" @mouseover="showDeleteButton = true"
+  <div v-show="show" class="bg-white rounded-lg shadow-lg mb-4 relative" @mouseover="showDeleteButton = true"
     @mouseleave="showDeleteButton = false">
     <div v-show="editable" @click="deleteMessage">
       <button v-show="showDeleteButton"
@@ -25,25 +25,40 @@
       <img v-if="message.media" class="w-full rounded-lg shadow-lg mb-4" :src="this.apiUrl + '/media/' + message.media"
         alt="Media attached to the message" />
       <p class="text-gray-800 text-base">{{ aMessage.content }}</p>
-      <div class="flex items-center mt-4">
-        <button class="flex items-center text-gray-500 hover:text-red-500 mr-4" @click="toggleLike">
-          <svg :class="{ 'text-red-500': liked, 'text-black': !liked }" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-            viewBox="0 0 24 24" fill="currentColor">
-            <path
-              d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z">
-            </path>
-          </svg>
-          <span :class="{ 'text-red-500': liked, 'text-black': !liked }" class="ml-2">{{ likeCount }}</span>
-        </button>
+
+      <div class="bottom grid  grid-cols-2 gap-4 mt-4">
+        <div class="flex items-center ">
+          <button class="flex items-center text-gray-500 hover:text-red-500 mr-4" @click="toggleLike">
+            <svg :class="{ 'text-red-500': liked, 'text-black': !liked }" xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+              <path
+                d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z">
+              </path>
+            </svg>
+            <span :class="{ 'text-red-500': liked, 'text-black': !liked }" class="ml-2">{{ likeCount }}</span>
+          </button>
+        </div>
+        <div class="flex items-center ">
+          <button class="flex items-center text-gray-500 hover:text-red-500 mr-4" @click="replyToMessage">
+            <svg :class="{ 'text-blue-400': replied, 'text-black': !replied }" xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+              <path clip-rule="evenodd" fill-rule="evenodd" d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902 1.168.188 2.352.327 3.55.414.28.02.521.18.642.413l1.713 3.293a.75.75 0 001.33 0l1.713-3.293a.783.783 0 01.642-.413 41.102 41.102 0 003.55-.414c1.437-.231 2.43-1.49 2.43-2.902V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0010 2zM6.75 6a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 2.5a.75.75 0 000 1.5h3.5a.75.75 0 000-1.5h-3.5z"></path>
+            </svg>
+            <span :class="{ 'text-blue-400': replied, 'text-black': !replied }" class="ml-2">{{ messageCount }}</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
+
+
 </template>
 
 
 <script>
 import config from "@/config";
 import axios from "axios";
+
 
 export default {
   props: {
@@ -59,14 +74,18 @@ export default {
       type: String,
       required: true
     },
-    editable:{
-      type:Boolean,
+    editable: {
+      type: Boolean,
       required: true
     },
-    allowVistingProfile:{
-            type: Boolean,
-            required: true
-        }
+    allowVistingProfile: {
+      type: Boolean,
+      required: true
+    },
+    replied: {
+      type: Boolean,
+      default: true,
+    }
   },
   data() {
     return {
@@ -74,9 +93,12 @@ export default {
       apiUrl: this.url,
       liked: false,
       likeCount: this.message.likes.length,
+      messageCount: 3,
       nameOfUser: this.username,
       showDeleteButton: false,
-      show: true
+      show: true,
+
+      isModalVisible: false,
     };
   },
   mounted() {
@@ -90,9 +112,9 @@ export default {
     }
 
     //set like to server input
-    try{
+    try {
       this.liked = this.message.liked
-    } catch{
+    } catch {
       this.liked = false;
     }
 
@@ -133,15 +155,19 @@ export default {
 
 
       axios.delete('/messages/' + this.aMessage._id)
-        .then(response =>{
-          this.show= false;
+        .then(response => {
+          this.show = false;
         })
 
 
     },
-    visitProfile(name){
-      if(this.allowVistingProfile){   this.$router.push('profile/?user=' + name)}
+    visitProfile(name) {
+      if (this.allowVistingProfile) { this.$router.push('profile/?user=' + name) }
 
+
+    },
+    replyToMessage(){
+      this.$emit('reply', this.aMessage.user._id);
 
     }
   }
