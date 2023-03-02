c05eb8496275af45bea833f3918e8c679b396ad2
Thijmen <yeaaccount@gmail.com>
2023-03-02 12:37:40 +0100

    no overflow

--- app/landingPage/components/homeContent.vue
+++ app/landingPage/components/homeContent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="logo lg:grid lg:grid-cols-9">
+  <div class="overflow-hidden">
+      <div class="logo lg:grid lg:grid-cols-9 ">
     <div
       class=" lg:col-span-7 logo w-full flex h-[166px] md:h-[254px] lg:h-[283px] justify-center items-center bg-white rounded-tr-full rounded-br-full mt-[83px] lg:mt-[170px] ">
       <p class="text-[70px] md:text-[120px] pr-12">ZENOX</p>
@@ -7,10 +8,13 @@
   </div>
 
   <p
-    class="bottom-text w-10 mt-[90px]  lg:mt-[100px] text-[55px] md:text-[70px] md:leading-[80px] lg:text-[80px] lg:leading-[100px] leading-[60px] font-bold text-white items-start text-start pl-3">
+    class="  w-fit h-full fixed overflow-clip
+    bottom-text  mt-[90px]  lg:mt-[100px] text-[55px] md:text-[70px] md:leading-[80px] lg:text-[80px] lg:leading-[100px] leading-[60px] font-bold text-white items-start text-start pl-3">
     Alternative <br>
     Social<br>
     Media<br>
   </p>
+
+  </div>
 
 </template>
