468e1016a7d53001d6325379b0e32a94d618299e
Thijmen <yeaaccount@gmail.com>
2023-03-04 16:18:41 +0100

    -console.log

--- app/zenox/components/messagesList.vue
+++ app/zenox/components/messagesList.vue
@@ -39,9 +39,6 @@ export default {
     },
 
     methods: {
-        created(){
-            console.log(this.messages)
-        },
         openReplies(e) {
             this.$router.push('/replies?message=' + e)
         },
