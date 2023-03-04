738da55235b64822b5502d22c71a114843b0c0e4
Thijmen <yeaaccount@gmail.com>
2023-03-04 16:20:54 +0100

    fixed stlying

--- app/zenox/views/Replies.vue
+++ app/zenox/views/Replies.vue
@@ -1,36 +1,38 @@
 <template>
-
   <!-- TODO: add that people can remove their own comments -->
-  <div class="container-fluid flex flex-row items-center">
-    <main-sidebar />
+  <div class="container-fluid h-full w-full items-center font-medium">
 
-    <div class="flex flex-col md:flex-row h-screen flex-1 ">
-      <div class="profile min-w-full flex flex-col items-center">
-        <div class="flex flex-row items-center justify-between w-full px-4 py-2 bg-gray-100  ">
-          <button @click="$router.go(-1)" class="rounded-full hover:bg-gray-200 px-2 py-1">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 fill-current">
-              <path clip-rule="evenodd" fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z"></path>
 
-            </svg>
-          </button>
-          <h2 class="text-lg font-semibold mb-4">Replies</h2>
-        </div>
-        <div class="overflow-scroll scrollbar-hide ">
-          <message-box @messageSend="sendReply" class="md:mx-16 w-[300px] md:w-[400px] lg:w-[600px] pt-20" :header="this.messageBoxHeader"></message-box>
-        <div class="flex flex-col flex-1 mx-4 md:mx-16 w-[320px] md:w-[400px] lg:w-[600px] pt-8 ">
-          <messages-list  :allowReplying=false :messages="messages"></messages-list>
-        </div>
-        </div>
-        <!-- TODO: disable in back-end so you can't reply -->
+    <div class="flex flex-col content-center items-center justify-center w-full ">
 
+
+      <div class="flex items-center justify-between w-full px-4 bg-gray-100  ">
+        <button @click="$router.go(-1)" class="rounded-full hover:bg-gray-200 px-2 py-1">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 fill-current">
+            <path clip-rule="evenodd" fill-rule="evenodd"
+              d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z">
+            </path>
+
+          </svg>
+        </button>
+        <h2 class="text-lg font-semibold mb-4">Replies</h2>
       </div>
+
+      <message-box @messageSend="sendReply" class="width-formatting " :header="this.messageBoxHeader"></message-box>
+
+      <!-- TODO: disable in back-end so you can't reply again-->
+      <messages-list class="width-formatting" :allowReplying=false :messages="messages"></messages-list>
+
+
+
+
+
     </div>
   </div>
 </template>
 
 
-<style>
-</style>
+<style></style>
 
 <script>
 import MainSidebar from "@/zenox/components/mainSidebar.vue";
@@ -59,7 +61,6 @@ export default {
     sendReply(e) {
       // Do something with the reply here
 
-      console.log(e);
       const url = '/messages/' + this.messageId + '/reply'
       axios.post(url, e, {
         headers: {
@@ -78,23 +79,21 @@ export default {
       console.log(this.message._id);
       this.$emit("close");
     },
-    updateMessages(){
+    updateMessages() {
       const message = this.$route.query.message
-    axios
-      .get('/messages/replies/' + message)
-      .then((response) => {
-        this.messages = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      axios
+        .get('/messages/replies/' + message)
+        .then((response) => {
+          this.messages = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
     }
   },
   created() {
     this.updateMessages();
-
-
   },
 }
 </script>
