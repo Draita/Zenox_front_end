b35ea053a1e1e0723a64ddcef54ffe17725bfed0
Thijmen <yeaaccount@gmail.com>
2023-03-06 17:07:35 +0100

    updated tailwind css

--- app/zenox/views/Feed.vue
+++ app/zenox/views/Feed.vue
@@ -1,14 +1,12 @@
 <template>
 
 
-    <div class="container-fluid  flex  flex-row items-center text-white">
-        <main-sidebar  />
-
-        <div class="flex flex-col md:flex-row h-screen flex-1 overflow-scroll">
+<div class=" flex flex-col w-full md:justify-center md:flex-row  text-white">
+      <div class="profile flex width-formatting flex-col items-center">
             <div class="profile min-w-full flex flex-col items-center">
                 <!-- <message-box class="min-w-full" /> -->
-                <div class="flex flex-col flex-1 mx-4 md:mx-16 w-[300px] md:w-[400px] lg:w-[600px]">
-                    <h2 class="text-lg font-semibold mb-4 text-center">Posts from users you follow</h2>
+                <div class="flex flex-col flex-1  width-formatting">
+                    <h2 class="text-lg font-semibold mb-4 pl-[20px]">Post from users you follow</h2>
                     <messages-list class = "border-t-[1px] border-white" :messages="messages">
                     </messages-list>
                 </div>
@@ -16,6 +14,8 @@
         </div>
     </div>
 </template>
+
+
 
 
 <script>
