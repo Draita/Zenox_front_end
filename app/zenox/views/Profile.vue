0adc342104b5ec9e4d8317d804c4501460e50c32
Thijmen <yeaaccount@gmail.com>
2023-03-06 17:08:07 +0100

    changed fill to cover with banner

--- app/zenox/views/Profile.vue
+++ app/zenox/views/Profile.vue
@@ -1,94 +1,99 @@
 <template>
-    <loading v-if="loading" />
+    <div class="id" id="profile">
+        <loading v-if="loading" />
 
-    <div v-else="!loading" class="container-fluid h-full w-full items-center  font-medium  ">
+        <div v-else="!loading" class="container-fluid h-full w-full items-center  font-medium  ">
 
-        <div class="flex justify-center w-full">
+            <div class="flex justify-center w-full">
 
 
 
-            <div class=" w-full">
-                <!-- header -->
+                <div class=" w-full">
+                    <!-- header -->
 
-                <!-- profile header -->
+                    <!-- profile header -->
 
-                <div class="w-full  h-[246px]">
-                    <div class="w-full flex justify-center">
-                        <img class="h-[146px] w-[510px]  border-white border-l-[1px] border-r-[1px] object-fill"
-                            :src="this.user.banner">
+                    <div class="w-full  h-[246px]">
+                        <div class="w-full flex justify-center">
+                            <img class="h-[146px] w-[510px]  border-white border-l-[1px] border-r-[1px] object-cover
+                            "
+                                :src="this.user.banner">
 
-                    </div>
+                        </div>
 
-                    <div class=" grid-flow-col h-[62px]  bg-white w-full ">
-                        <!-- profile Picture -->
-                        <div class="flex justify-center content-center items-center  ">
-                            <div class=" grid grid-cols-2 w-[510px]  border-black border-l-[1px]">
-                                <div class="profile-picture flex  justify-center w-full mt-[-70px]">
-                                    <img class="h-[133px] w-[133px]
+                        <div class=" grid-flow-col h-[62px]  bg-white w-full ">
+                            <!-- profile Picture -->
+                            <div class="flex justify-center content-center items-center  ">
+                                <div class=" grid grid-cols-2 w-[510px]  border-black border-l-[1px]">
+                                    <div class="profile-picture flex  justify-center w-full mt-[-70px]">
+                                        <img class="h-[133px] w-[133px]
                                                                           rounded-full border-[4px] border-white"
-                                        :src="this.user.profilePicture">
-                                </div>
+                                            :src="this.user.profilePicture">
+                                    </div>
 
-                                <div class=" h-full background-grid-black  text-black border-black border-r-[1px]">
-                                    <div class="content flex h-full justify-center items-center opacity-100">
-                                        <svg class="ml-[5px]" width="30" height="30" viewBox="0 0 42 42" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M21 0L22.7334 8.94398L26.9164 0.850647L26.0598 9.92068L32.3535 3.33368L28.9762 11.795L36.8707 7.24792L31.2465 14.415L40.1023 12.2763L32.6866 17.5685L41.7863 18.0114L33.18 21L41.7863 23.9886L32.6866 24.4315L40.1023 29.7237L31.2465 27.585L36.8707 34.7521L28.9762 30.205L32.3535 38.6663L26.0598 32.0793L26.9164 41.1494L22.7334 33.056L21 42L19.2666 33.056L15.0836 41.1494L15.9402 32.0793L9.64654 38.6663L13.0238 30.205L5.12926 34.7521L10.7535 27.585L1.89773 29.7237L9.31338 24.4315L0.213749 23.9886L8.82 21L0.213749 18.0114L9.31338 17.5685L1.89773 12.2763L10.7535 14.415L5.12926 7.24792L13.0238 11.795L9.64654 3.33368L15.9402 9.92068L15.0836 0.850647L19.2666 8.94398L21 0Z"
-                                                fill="black" />
-                                        </svg>
-                                        <!-- profile content and button -->
-                                        <div class="button-and-text w-[140px]">
-                                            <p class="text-[20px] font-bold">{{ user.username }}</p>
-                                            <button class=" bg-black text-white w-[119px]
+                                    <div class=" h-full background-grid-black  text-black border-black border-r-[1px]">
+                                        <div class="content flex h-full justify-center items-center opacity-100">
+                                            <svg class="ml-[5px]" width="30" height="30" viewBox="0 0 42 42" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path
+                                                    d="M21 0L22.7334 8.94398L26.9164 0.850647L26.0598 9.92068L32.3535 3.33368L28.9762 11.795L36.8707 7.24792L31.2465 14.415L40.1023 12.2763L32.6866 17.5685L41.7863 18.0114L33.18 21L41.7863 23.9886L32.6866 24.4315L40.1023 29.7237L31.2465 27.585L36.8707 34.7521L28.9762 30.205L32.3535 38.6663L26.0598 32.0793L26.9164 41.1494L22.7334 33.056L21 42L19.2666 33.056L15.0836 41.1494L15.9402 32.0793L9.64654 38.6663L13.0238 30.205L5.12926 34.7521L10.7535 27.585L1.89773 29.7237L9.31338 24.4315L0.213749 23.9886L8.82 21L0.213749 18.0114L9.31338 17.5685L1.89773 12.2763L10.7535 14.415L5.12926 7.24792L13.0238 11.795L9.64654 3.33368L15.9402 9.92068L15.0836 0.850647L19.2666 8.94398L21 0Z"
+                                                    fill="black" />
+                                            </svg>
+                                            <!-- profile content and button -->
+                                            <div class="button-and-text w-[140px]">
+                                                <p class="text-[20px] font-bold">{{ user.username }}</p>
+                                                <button class=" bg-black text-white w-[119px]
                                                                           inner-border-0 border-black hover:inner-border-1 hover:bg-white hover:text-black
                                                                           transition duration-300 ease-in-out  " @click="profileButtonClick
                                                                           ">{{ profileButtonText }}</button>
+                                            </div>
                                         </div>
                                     </div>
+
                                 </div>
 
+
+                            </div>
+                            <!-- TODO: remove margin -1 -->
+                            <div class="bio h-[40px]  mt-[-1px] bg-white  flex justify-center">
+                                <div
+                                    class="w-[510px] background-bio    h-full  z-10 border-black border-t-[1px] border-r-[1px] border-l-[1px] ">
+                                    <p class="text-start pl-[11px] h-full text-[12px] pt-[6px] pr-[6px]  overflow-hidden ">
+                                        HI
+                                        {{ user.description }}</p>
+                                </div>
                             </div>
 
 
                         </div>
-                        <!-- TODO: remove margin -1 -->
-                        <div class="bio h-[40px]  mt-[-1px] bg-white  flex justify-center">
-                            <div
-                                class="w-[510px] background-bio    h-full  z-10 border-black border-t-[1px] border-r-[1px] border-l-[1px] ">
-                                <p class="text-start pl-[11px] h-full text-[12px] pt-[6px] pr-[6px]  overflow-hidden "> HI
-                                    {{ user.description }}</p>
-                            </div>
-                        </div>
-
-
                     </div>
+
+
+
+                    <message-box v-show="editable" @messageSend="sendMessage" class="" />
+
+
+
+
+                    <messages-list :editable="editable" :allowVistingProfile=false :messages=this.messages>
+                    </messages-list>
+
+
+
+
+
+
+
                 </div>
-
-
-
-                <message-box v-show="editable" @messageSend="sendMessage" class="" />
-
-
-
-
-                <messages-list :editable="editable" :allowVistingProfile=false :messages=this.messages>
-                </messages-list>
-
-
-
-
-
-
-
             </div>
+
+
+
         </div>
-
-
+        <change-profile v-if="isModalVisible" @close="hideModal()" :user="this.user"
+            @profileUpdated="updateProfile(user.username)" />
 
     </div>
-    <change-profile v-if="isModalVisible" @close="hideModal()" :user="this.user"
-        @profileUpdated="updateProfile(user.username)" />
 </template>
 
 <script>
@@ -139,8 +144,11 @@ export default {
 
 
     created() {
+        // BUG CAN ACCOR WHEN SWITCHING FROM PROFILE MODAL to another view to back to here where you can't scroll
+        //this fixes it lol
+        document.documentElement.style.overflow = 'auto'
 
-        const name = this.$route.query.user;
+        const name = this.$route.params.username;
         this.updateMessages
 
         this.checkFollow(name);
@@ -191,7 +199,7 @@ export default {
         //used when profile is selected on the main sidebar when already visitng a profile
         //otherwise the profile won't update
         refreshProfile() {
-            const name = this.$route.query.user
+            const name = this.$route.params.username;
             this.getMessages(name)
             this.updateProfile(name)
         },
